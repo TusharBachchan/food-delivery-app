6f6a0f84e00b0586253d1a51e78410a68a6f0cdf
tusharbachchan <tusharbachchan@gmail.com>
2023-08-08 06:51:30 +0530

    added scripts in package.json

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-const parent = React.createElement("h1", {}, "Hi!")
+// const parent = React.createElement("h1", {}, "Hi!")
+const parent = <h1>Hi!</h1>
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(parent)
